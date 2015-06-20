9b0a21970753d6a5d8fa987b6835be9b63c1e42e
Roy Haggerty <roy.haggerty@oregonstate.edu>
2015-06-19 21:01:02 -0700

    working on getting drainage area

--- download_gage_data.py
+++ download_gage_data.py
@@ -14,11 +14,12 @@ sys.path.append('c:\\code\\nwispy\\')
 from nwispy import nwispy_webservice
 #print user_parameters_url
 #assert False
-pathy = 'C:\\code\\WBgages\\'
+pathy = 'C:\\code\\Willamette Basin gauge data\\'
 #pathy = 'C:\\code\\'
-Q_SWE_PRE_params = xlrd.open_workbook('C:\\code\\maplot\\Q-SWE-PRE_temp.xlsx')
-#Gauge_num = Q_SWE_PRE_params.sheet_by_index(2).col_values(0)[1:]
-Gauge_num = [14144800]
+Q_SWE_PRE_params = xlrd.open_workbook('C:\\code\\Willamette Basin gauge data\\Q-SWE-PRE_temp.xlsx')
+#Gauge_num = Q_SWE_PRE_params.sheet_by_index(2).col_values(0)[1:]  # for whole list of gages
+Gauge_num = [14200100]  # for specific gage
+# TRY gage 14200100
 b = []
 for gage in Gauge_num:
     gage = int(gage)
@@ -43,6 +44,23 @@ for gage in Gauge_num:
     b.append(dd[2:][0])
     print dd[2:][0]
 
+    #XML extract code to pull drainage area
+    from lxml import html
+    import requests
+    page = requests.get("http://nwis.waterdata.usgs.gov/nwis/inventory?search_site_no="+
+    gage_num_str+
+    "&search_site_no_match_type=exact&site_tp_cd=ST&drain_area_va_conjunction=and&group_key=county_cd&format=sitefile_output&sitefile_output_format=xml&column_name=agency_cd&column_name=site_no&column_name=station_nm&column_name=site_tp_cd&column_name=huc_cd&column_name=drain_area_va&list_of_search_criteria=search_site_no%2Csite_tp_cd%2Cdrain_area_va")
+#    print page.text
+#    assert False
+#    print "http://nwis.waterdata.usgs.gov/nwis/inventory?search_site_no="+   gage_num_str+\
+#    "&search_site_no_match_type=exact&site_tp_cd=ST&drain_area_va_conjunction=and&group_key=county_cd&format=sitefile_output&sitefile_output_format=xml&column_name=agency_cd&column_name=site_no&column_name=station_nm&column_name=site_tp_cd&column_name=huc_cd&column_name=drain_area_va&list_of_search_criteria=search_site_no%2Csite_tp_cd%2Cdrain_area_va"
+#    assert False
+    tree = html.fromstring(page.content)
+    da = (tree.text_content()[-9:-3])
+    print da, 'here'
+    assert False
+    drain_area = tree.label
+
 with open(pathy+"gage_locations.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(b)
@@ -59,7 +77,6 @@ with open(pathy+filey) as f:
 dd = []
 for line in d:
     if line[0][0][0] != '#': dd.append(line[2:4])
-import numpy as np
 a = np.array(dd[2:])
 
 
